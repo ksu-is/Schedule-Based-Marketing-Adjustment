a86907604ded0556f0d7ce75264841dfd491e673
jsmi1498 <142547794+jsmi1498@users.noreply.github.com>
2023-11-18 21:42:37 -0500

    Update Project.py

--- Project.py
+++ Project.py
@@ -1,6 +1,6 @@
 from appointmentCreator import enter_data
 
 if enter_data == "":
-    print ("Decrease marketing spending")
-elif enter_data in time:
     print ("Increase marketing spending")
+elif enter_data in time:
+    print ("Decrease marketing spending")
